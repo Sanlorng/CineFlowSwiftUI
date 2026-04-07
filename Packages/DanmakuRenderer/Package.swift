9fb18929ab4f9c82c3aa7ecddef52d202ab95c78
Sanlorng <fujia625@gmail.com>
2026-04-07 23:07:31 +0800

    Update danmaku support version

--- Packages/DanmakuRenderer/Package.swift
+++ Packages/DanmakuRenderer/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "DanmakuRenderer",
     platforms: [
-        .macOS(.v13),
+        .macOS(.v14),
         .iOS(.v16),
         .tvOS(.v16),
         .visionOS(.v1)
